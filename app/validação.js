58ae7c9227f0084f8dbe889c67d3372581961d50
Fabiano Raiser <fabiano.raiser16@gmail.com>
2023-01-18 13:17:10 -0300

    Add uma tela de game over

--- app/validação.js
+++ app/validação.js
@@ -2,7 +2,17 @@ function verificaChuteValorValido(chute){
     const numero = +chute
 
     if (chuteForInvalido(numero)) {
-        elementoChute.innerHTML += `<div>Valor Inválido</div>`
+        if (chute.toUpperCase() === "GAME OVER") {
+            document.body.innerHTML = 
+            `
+            <h2>Game Over!!!</h2>
+            <h3>Pressione o botão para jogar novamente</h3>
+            <button id="jogar-novamente" class="btn-jogar" >Jogar Novamente</button>
+            `
+            document.body.style.backgroundColor = "black"
+        } else {
+            elementoChute.innerHTML += `<div>Valor Inválido</div>`
+        }
         return
     }
 
@@ -13,6 +23,7 @@ function verificaChuteValorValido(chute){
         return
     }
 
+
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
@@ -20,6 +31,7 @@ function verificaChuteValorValido(chute){
 
             <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
         `
+    
     } else if(numero > numeroSecreto ) {
         elementoChute.innerHTML += `
         <div>O númeor secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
@@ -29,6 +41,7 @@ function verificaChuteValorValido(chute){
         <div>O númeor secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
         `
     }
+
 }
 
 function chuteForInvalido(numero) {
